96082b8d254f4a2e0126333bc9ca16216de486b9
Avinash Gupta <avinash.gupta@vils.ai>
2023-05-17 17:35:57 +0530

    tailwindcss configurations

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,17 @@
 const Home = () => {
-  return (
-    <div className="home-container center">
-      <h1>
-        <i
-          className="fa-solid fa-bolt"
-          style={{
-            marginRight: "15px",
-          }}
-        />
-        Hi, welcome to avinash templates/react
-      </h1>
-    </div>
-  );
+    return (
+        <div className="bg-slate-800 h-screen text-white grid place-content-center">
+            <h1>
+                <i
+                    className="fa-solid fa-bolt"
+                    style={{
+                        marginRight: "15px"
+                    }}
+                />
+                Hi, welcome to avinash templates/react
+            </h1>
+        </div>
+    );
 };
 
 export default Home;
