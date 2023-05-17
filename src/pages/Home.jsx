b30bc1d752c7f9327f9ca90d903ac7bbd903d789
Avinash Gupta <avinash.gupta@vils.ai>
2023-05-17 16:27:37 +0530

    Initial Commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+const Home = () => {
+  return (
+    <div className="home-container center">
+      <h1>
+        <i
+          className="fa-solid fa-bolt"
+          style={{
+            marginRight: "15px",
+          }}
+        />
+        Hi, welcome to avinash templates/react
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
