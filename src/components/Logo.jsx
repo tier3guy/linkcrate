6951d45be74b22ca5fe0d7c622596e99ea8facad
Avinash Gupta <avinash.gupta@vils.ai>
2023-05-21 09:34:33 +0530

    Logo Beta

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -6,13 +6,16 @@ import { Link } from "react-router-dom";
 
 const Logo = ({ label, svgOnly, labelStyle, logoStyle }) => {
     return (
-        <Link to="/">
+        <Link to="/" className="">
             <div className="flex space-x-2 items-center">
                 <LogoSVG className={`h-8 w-8 ${logoStyle}`} />
                 {!svgOnly && (
-                    <p className={`text-2xl ${labelStyle}`}>
-                        {label || "Linkcrate"}
-                    </p>
+                    <div>
+                        <p className={`text-2xl ${labelStyle}`}>
+                            {label || "Linkcrate"}
+                        </p>
+                        <p className="text-[14px] -mt-1 text-gray-400 ">Beta</p>
+                    </div>
                 )}
             </div>
         </Link>
